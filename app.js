a0d3872fbd68418326fc2528c466dbe46ae4e522
ameer hamza <ameerhamzabutt555@gmail.com>
2024-10-08 19:45:39 +0500

    region add cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://myassessmentapp.netlify.app/', // Replace with your frontend's URL
+    origin: 'https://myassessmentapp.netlify.app', // Replace with your frontend's URL
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'], // Add any custom headers you might be using
 }));
